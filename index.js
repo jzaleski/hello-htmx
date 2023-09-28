6ac48e86621edc410d5ac2c28fac1730ddb3ca67
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-09-28 14:28:11 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ?? 3001;
+
+const app = express();
+
+app.use(function(req, res, next) {
+  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect(302, `https://${req.hostname}${req.originalUrl}`);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html');
+});
+
+app.post('/api/clicked', (req, res) => {
+  res.send('Clicked!');
+});
+
+app.listen(port, host, () => console.log(`Server listening at: http://${host}:${port}`));
